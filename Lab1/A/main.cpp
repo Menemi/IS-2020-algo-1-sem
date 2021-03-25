970e4dc9e5ff5740dd099eb1ffaef56c951f508c
DAH9 <danila.titov1210@yandex.ru>
2021-03-26 01:52:24 +0300

    Initial commit

---
+++ Lab1/A/main.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    ifstream fin("aplusbI.txt");
+    ofstream fout("aplusbO.txt");
+    fin >> a >> b;
+    fout << a + b << "\n";
+    return 0;
+}
